618ac7c786b5364901a5db6943c61a13cd162048
BG7LIF <zmissu@163.com>
2026-02-24 01:35:50 +0800

    Initial commit

---
+++ src/main/java/me/dqq/leadmin/LeAdminApplication.java
@@ -0,0 +1,13 @@
+package me.dqq.leadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dqq/leadmin/LeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dqq.leadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
